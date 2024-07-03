a7897d15011f6b89f50b3bb99b7f28869caf609a
STeerajit <6520301001@cdti.ac.th>
2024-07-03 11:51:49 +0700

    answer01.py

--- assignment4/asyncio01.py
+++ assignment4/asyncio01.py
@@ -1,1 +1,15 @@
-# example of getting the current task from the main coroutine
+import asyncio
+
+# Define the main coroutine
+async def main():
+    # Report a message
+    print("main coroutine started")
+    
+    # Get the current task
+    task = asyncio.current_task()
+    
+    # Report its details
+    print(task)
+
+# Start the asyncio program
+asyncio.run(main())
